0ef0263050eac99bdd23d3da9b7eb01145308832
Denis Espino <despino6160.com>
2023-05-31 19:05:03 -0400

    added main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "test-rsg-001" {
+  name = "test-rsg-001"
+  location = "uc"
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,6 +4,6 @@ provider "azurerm" {
   }
   subscription_id = "a6fe9d63-5634-493a-bbc2-72641872daf6"
   client_id = var.client-id
-  client_secret = var.secret
+  secret = var.secret
   tenant_id = var.tenant-id
 }
